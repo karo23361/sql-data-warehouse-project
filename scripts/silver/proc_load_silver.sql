4bb2ad831eedd3d9997df200046e94c8ae65bdd0
karo23361 <118350099+karo23361@users.noreply.github.com>
2025-04-09 11:10:31 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -26,3 +26,34 @@ SELECT
 *,
 ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
 FROM bronze.crm_cust_info WHERE cst_id IS NOT NULL)t WHERE flag_last = 1 
+
+
+
+
+INSERT INTO silver.crm_prd_info(
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+
+SELECT 
+  prd_id,
+  REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') as cat_id,
+  SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+  prd_nm,
+  ISNULL(prd_cost, 0) AS prd_cost,
+  CASE UPPER(TRIM(prd_line))
+	  WHEN 'M' THEN 'Mountain'
+	  WHEN 'R' THEN 'Road'
+	  WHEN 'S' THEN 'Other Sales'
+	  WHEN 'M' THEN 'Touring'
+	  ELSE 'n/a'
+  END AS prd_line,
+  CAST (prd_start_dt AS DATE) AS prd_start_dt,
+  CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE ) AS prd_end_dt
+FROM bronze.crm_prd_info
