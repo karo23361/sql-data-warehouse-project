ca8afb7917cdb2c94046bc0d03eed5c81685f082
Karol Szeląg <118350099+karo23361@users.noreply.github.com>
2025-04-14 18:28:37 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -117,3 +117,18 @@ CASE
 	ELSE 'n/a'
 END AS gen
 FROM bronze.erp_cust_az12;
+
+
+
+
+INSERT INTO silver.erp_loc_a101 
+(cid, cntry)
+
+SELECT 
+REPLACE(cid, '-', '') cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	 WHEN TRIM(cntry) IN ('USA', 'US') THEN 'United States'
+	 WHEN TRIM(cntry) = '' OR cntry is NULL THEN 'n/a'
+	 ELSE TRIM(cntry)
+END AS cntry
+FROM bronze.erp_loc_a101
